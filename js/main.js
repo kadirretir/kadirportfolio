aaa646e57260a9e47940868a635dad1685967e30
kadirretir <kobraninkilici@gmail.com>
2022-04-11 21:11:21 +0300

    about button linked

--- js/main.js
+++ js/main.js
@@ -2,5 +2,3 @@ gsap.from("#hero-image", {opacity: 0, duration: 1, x: -50, delay: 1.1 });
 gsap.from(".hero-right", {opacity: 0, duration: 1, x: 50, delay: 0.4});
 gsap.from(".navbar", {opacity: 0, duration: 3, delay: 0.4});
 gsap.from(".cv-section-middle", {opacity: 0, duration: 1, y: 50, delay: 1.8 });
-
-
